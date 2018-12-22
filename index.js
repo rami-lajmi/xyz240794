940cf4f8f67bc61fb542e3c89493ab0d13ff0617
Rami Lajmi <rami.lajmi@ublu.fr>
2018-12-22 02:39:34 +0100

    add modif6

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get("/", function (req, res) {
 // Facebook Webhook
 // Used for verification
 app.get("/webhook", function (req, res) {
-  if (req.query["hub.verify_token"] === process.env.VERIFICATION_TOKEN) {
+  if (req.query["hub.verify_token"] === "EAAgJFzaHm4kBAGW0pPh8OyNkvy4vrGXfYiXbtlI4HFXSsSlYarhNVqTwHuTZCRPvsNJQJaD9UOfk46yahg5RPAtiPAtOZBzpJwDyu56lmasUW37knOGwZAWnlHjzmhxKoTQsqvIDT6IK9oxpmQMLypYjL9NqdoWAUCwGHFIcQZDZD") {
     console.log("Verified webhook");
     res.status(200).send(req.query["hub.challenge"]);
   } else {
